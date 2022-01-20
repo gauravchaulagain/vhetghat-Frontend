70a2fdebd41c69f532422d78e78e87c6c6820a2e
gauravchaulagain <gauravchaulagain45@gmail.com>
2022-01-20 21:20:52 +0545

    page render

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import PageRender from './PageRender'
-
-
-
+import PageRender from "./PageRender";
 
 function App() {
   return (
     <router>
-    <input type="checkbox" id="theme"/>
+      <input type="checkbox" id="theme" />
       <div className="App">
         <div className="main">
-          <Route path="/:page" component = {PageRender} />
+          <Route exact path="/:page" component={PageRender} />
+          <Route exact path="/:page:id" component={PageRender} />
         </div>
       </div>
     </router>
--- src/PageRender.js
+++ src/PageRender.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import {useParams} from 'react-router-dom'
+import NotFound from '../components/NotFound'
+import { useSelector } from 'react-redux'
 
-const PageRender =() => {
-    const {page} = useParams()
-    console.log(useParams())
-    return (
-        <div>
-            
-        </div>
-    )
+const generatePage = (pageName) => {
+    const component = () => require(`../pages/${pageName}`).default
+
+    try {
+        return React.createElement(component())
+    } catch (err) {
+        return <NotFound />
+    }
+}
+
+const PageRender = () => {
+    const {page, id} = useParams()
+    const { auth } = useSelector(state => state)
+
+    let pageName = "";
+
+    if(auth.token){
+        if(id){
+            pageName = `${page}/[id]`
+        }else{
+            pageName = `${page}`
+        }
+    }
+
+    return generatePage(pageName)
 }
 
 export default PageRender
