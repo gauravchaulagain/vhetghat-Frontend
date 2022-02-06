ebb07c1fe4b086f87b87a859096df379fab6dad2
gauravchaulagain <gauravchaulagain45@gmail.com>
2022-02-06 12:51:39 +0545

    MOre on message

--- src/components/message/RightSide.js
+++ src/components/message/RightSide.js
@@ -26,3 +26,105 @@ const RightSide = () => {
             setPage(newData.page)
         }
     },[message.data, id])
+    useEffect(() => {
+        if(id && message.users.length > 0){
+            setTimeout(() => {
+                refDisplay.current.scrollIntoView({behavior: 'smooth', block: 'end'})
+            },50)
+
+            const newUser = message.users.find(user => user._id === id)
+            if(newUser) setUser(newUser)
+        }
+    }, [message.users, id])
+
+    const handleChangeMedia = (e) => {
+        const files = [...e.target.files]
+        let err = ""
+        let newMedia = []
+
+        files.forEach(file => {
+            if(!file) return err = "File does not exist."
+
+            if(file.size > 1024 * 1024 * 5){
+                return err = "The image/video largest is 5mb."
+            }
+
+            return newMedia.push(file)
+        })
+
+        if(err) dispatch({ type: GLOBALTYPES.ALERT, payload: {error: err} })
+        setMedia([...media, ...newMedia])
+    }
+
+    const handleDeleteMedia = (index) => {
+        const newArr = [...media]
+        newArr.splice(index, 1)
+        setMedia(newArr)
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        if(!text.trim() && media.length === 0) return;
+        setText('')
+        setMedia([])
+        setLoadMedia(true)
+
+        let newArr = [];
+        if(media.length > 0) newArr = await imageUpload(media)
+
+        const msg = {
+            sender: auth.user._id,
+            recipient: id,
+            text, 
+            media: newArr,
+            createdAt: new Date().toISOString()
+        }
+
+        setLoadMedia(false)
+        await dispatch(addMessage({msg, auth, socket}))
+        if(refDisplay.current){
+            refDisplay.current.scrollIntoView({behavior: 'smooth', block: 'end'})
+        }
+    }
+    useEffect(() => {
+        const getMessagesData = async () => {
+            if(message.data.every(item => item._id !== id)){
+                await dispatch(getMessages({auth, id}))
+                setTimeout(() => {
+                    refDisplay.current.scrollIntoView({behavior: 'smooth', block: 'end'})
+                },50)
+            }
+        }
+        getMessagesData()
+    },[id, dispatch, auth, message.data])
+
+
+    // Load More
+    useEffect(() => {
+        const observer = new IntersectionObserver(entries => {
+            if(entries[0].isIntersecting){
+                setIsLoadMore(p => p + 1)
+            }
+        },{
+            threshold: 0.1
+        })
+
+        observer.observe(pageEnd.current)
+    },[setIsLoadMore])
+
+    useEffect(() => {
+        if(isLoadMore > 1){
+            if(result >= page * 9){
+                dispatch(loadMoreMessages({auth, id, page: page + 1}))
+                setIsLoadMore(1)
+            }
+        }
+        // eslint-disable-next-line
+    },[isLoadMore])
+
+    const handleDeleteConversation = () => {
+        if(window.confirm('Do you want to delete?')){
+            dispatch(deleteConversation({auth, id}))
+            return history.push('/message')
+        }
+    }
